c9d945eaee9c2ac86cd5f02629f283c31d062ef1
simar-singh <55468002+simar-singh@users.noreply.github.com>
2020-07-19 18:05:45 -0400

    Readded FA Icons

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,14 @@ import router from './router'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import {
+ faChevronDown
+} from '@fortawesome/free-solid-svg-icons'
+
+library.add(faChevronDown)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.config.productionTip = false
 Vue.use(BootstrapVue)
