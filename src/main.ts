911bd50b79535bce24d3823cc5f0d68c662a023d
Simar Singh <simarsingh@Simars-MBP.fios-router.home>
2020-07-18 21:59:13 -0400

    Changed main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,11 @@ import router from './router'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
-import VueSidebarMenu from 'vue-sidebar-menu'
+
 import 'vue-sidebar-menu/dist/vue-sidebar-menu.css'
 
 Vue.config.productionTip = false
 Vue.use(BootstrapVue)
-Vue.use(VueSidebarMenu);
 
 new Vue({
   router,
