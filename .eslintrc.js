1b6b8db20e57f799c6a9085983d3b60a03b618ab
wusatosi <26424577+wusatosi@users.noreply.github.com>
2020-07-26 02:44:32 +0000

    using Airbnb's code style profile

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2020: true,
+  },
+  extends: [
+    'plugin:vue/essential',
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 11,
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module',
+  },
+  plugins: [
+    'vue',
+    '@typescript-eslint',
+  ],
+  rules: {
+  },
+};
