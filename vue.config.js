12aa9c8203e8dba1cc11a4ba63254de56cdfc531
wusatosi <26424577+wusatosi@users.noreply.github.com>
2020-08-01 20:47:50 +0800

    allow public path injection through environment variable

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,23 @@
-module.exports = {
-  devServer: {
-    allowedHosts: [
-      '.ws-us02.gitpod.io'
-    ]
+const getDevConfig = () => {
+  return {
+    devServer: {
+      allowedHosts: [
+        '.ws-us02.gitpod.io'
+      ]
+    }
   }
+}
+
+const getProductionConfig = () => {
+  return {
+    integrity: true,
+    publicPath: process.env["PUBLIC_PATH"],
+    crossorigin: "anonymous"
+  }
+}
+
+if (process.env.NODE_ENV === "production") {
+  module.exports = getProductionConfig();
+} else {
+  module.exports = getDevConfig();
 }
