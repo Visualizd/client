87d204b774b39dd997f2487819711f30bc759585
wusatosi <26424577+wusatosi@users.noreply.github.com>
2020-08-09 18:00:14 +0800

    separate asset path with public path

--- vue.config.js
+++ vue.config.js
@@ -13,13 +13,37 @@ const getDevConfig = () => {
 }
 
 const getProductionConfig = () => {
-  const publicPath = process.env["PUBLIC_PATH"];
-  console.log(`Compiling with public path: ${publicPath}`);
-  return {
-    integrity: true,
-    publicPath: publicPath,
-    crossorigin: "anonymous"
-  }
+  const assetPath = process.env["ASSET_PATH"];
+  const deployPublicPath = process.env["PUBLIC_PATH"];
+  console.log(`Compiling with asset path: ${assetPath}, public path: ${deployPublicPath}`);
+
+  // files referenced in HTML template, is placed under the /public folder
+  // all files inside this folder will be copied over to deploy public path
+
+  // this piece of code overrides the BASE_URL template in the index.html template
+  // so that files referenced in the HTML template but not processed in the webpack system
+  // will have a correct reference url
+  const overrideBaseURLTemplate = {
+    chainWebpack: config => {
+      config.plugin("html").tap(args => {
+        const original = args[0].templateParameters;
+        args[0].templateParameters = (compilation, assets, pluginOptions) => {
+          return Object.assign(original(compilation, assets, pluginOptions), {
+            BASE_URL: deployPublicPath
+          })
+        }
+        return args;
+      })
+    }
+  };
+
+  return Object.assign({
+        integrity: true,
+        publicPath: assetPath,
+        crossorigin: "anonymous",
+      },
+      overrideBaseURLTemplate
+  )
 }
 
 if (process.env.NODE_ENV === "production") {
