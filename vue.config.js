baac7611822fe32193510d2fb95a39a6d006aa16
wusatosi <26424577+wusatosi@users.noreply.github.com>
2020-08-10 01:11:01 +0800

    Deploy pipeline based on GitHub pages  (#5)
    * production build script from build -> build:prod
    
    * allow public path injection through environment variable
    
    * updated vue config for asset folder separation
    
    * Incomplete github action setup
    
    * Fix GitHub action syntax
    
    * Attempt to fix ::set-env: command not found
    
    * Incomplete github action setup [2]
    
    * Fix path typo
    
    * Added commit-assests github action job
    
    * Separate commit assets to two steps for debug
    
    * Try to debug project structure
    
    * Don't do git branch --orphan
    
    * Fix syntax issue
    
    * attempt to fix with explicate branch info
    
    * Try not to commit if there's no changes present
    
    * Fix bash syntax
    
    * Deploy to gp-page branch
    
    * Attempt to fix bash
    
    * setup git name 0w0
    
    * assets should be in assets folder
    
    * update git add
    
    * update debugging output
    
    * Try to detect changes properly
    
    * Show more debug info
    
    * Try to hard wire git commit to see if git works
    
    * will the change detection work this time?
    
    * Clean up action file
    
    * Using bahmutov/npm-install to cache dependency
    
    * on push on all branch (for debug)
    
    * Try to fix artifact file name
    
    * try to make use of build cache
    
    * Non-fixed artifact name
    
    * Fix action environment variable synatax
    
    * Attempt to fix artifact file name again
    
    * separate asset path with public path
    
    * git add before commit
    
    * Correctly setup build cache
    
    * Move environmental variables to head of the file
    
    * Fix public path variable
    
    * Specify branch name as environmental variable
    
    * Reformatted code
    
    * Attempt to fix incorrect variable reference
    
    * Setup git name and email using environment var
    
    * Attempt to fix git name config
    
    * Attempt 2 to fix git name setting
    
    * Attempt 3 to fix git name setting
    
    * Attempt 3 to fix git name setting
    
    * Use another git bot email
    
    * Leave git bot email blank
    
    * Leave git bot email blank (2)
    
    * Add helpful description
    
    * Workflow only runs when master is updated
    
    * Reformat
    
    * Config vuejs to use hash router mode on github pages
    
    * Clean-up & added some documentations

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,81 @@
-module.exports = {
-  devServer: {
-    allowedHosts: [
-      '.ws-us02.gitpod.io'
-    ]
+// inject ROUTER_MODE from build environment variable to runtime
+// see: https://github.com/wusatosi/client/blob/1b15c03d4f50d59fbeec6e8397b68d40320e5b1b/.env#L23-L29
+function injectRouterModeEnvToRuntime(webpackConfig) {
+  webpackConfig.plugin("define").tap(defineConfig => {
+    const runtimeEnv = defineConfig[0]["process.env"];
+    const routerMode = JSON.stringify(process.env["ROUTER_MODE"] || "history");
+    runtimeEnv["ROUTER_MODE"] = routerMode;
+    console.log(`using router mode: ${routerMode}`);
+    return defineConfig;
+  })
+}
+
+const baseConfig = {
+  assetsDir: "assets",
+  chainWebpack: webpackConfig => {
+    injectRouterModeEnvToRuntime(webpackConfig);
   }
+}
+
+const getDevConfig = () => {
+  return {
+    devServer: {
+      allowedHosts: [
+        '.ws-us02.gitpod.io'
+      ]
+    }
+  }
+}
+
+const getProductionConfig = () => {
+  // this is where assets will be deployed, normally on jsdelivr's cdn
+  const assetPath = process.env["ASSET_PATH"];
+  // this is the main domain our site is hosted on, for example visualizd.github.io
+  const deployPublicPath = process.env["PUBLIC_PATH"];
+  console.log(`Compiling with asset path: ${assetPath}, public path: ${deployPublicPath}`);
+
+  // Files referenced directly in HTML template should be placed under the /public folder
+  // all files inside the /public/ directory will be copied over to deploy public path,
+  // on the same domain and prefix as index.html.
+  //          (ex: visualizd.github.io/client/ instead of cdn.jsdelivr.net)
+  // This following piece of code overrides the BASE_URL template in the index.html template
+  // so that files referenced in the HTML template but not processed in the webpack system
+  // will have a correct reference url
+  const overrideBaseURLTemplate = {
+    chainWebpack: config => {
+      config.plugin("html").tap(args => {
+        // vue-cli has this config set to a special function,
+        // to make sure build will work, this function is
+        // reserved
+        const original = args[0].templateParameters;
+        args[0].templateParameters = (compilation, assets, pluginOptions) => Object.assign(
+            // ensure the original function is reserved
+            original(compilation, assets, pluginOptions),
+            // overrides the base url to that of the deployment path instead of asset path
+            {
+              BASE_URL: deployPublicPath
+            }
+        )
+        return args;
+      })
+    }
+  };
+
+  return Object.assign({
+    // enable sub-resource integrity check as our executables are deployed on other's infra
+    // there were records where jsdelivr was poisoned
+    // see: https://github.com/jsdelivr/jsdelivr/issues/18049
+        integrity: true,
+        publicPath: assetPath,
+    // necessary for assests hosted by jsdelivr cdn to work as it counts as cors
+        crossorigin: "anonymous",
+      },
+      overrideBaseURLTemplate
+  )
+}
+
+if (process.env.NODE_ENV === "production") {
+  module.exports = Object.assign(baseConfig, getProductionConfig());
+} else {
+  module.exports = Object.assign(baseConfig, getDevConfig());
 }
