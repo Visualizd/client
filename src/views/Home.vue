bfef76a1b1803725c2e66ffb524564bb7cb38a7c
williamjallen <william.j.allen24@gmail.com>
2021-12-18 20:48:59 -0500

    progress

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+// import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
+//
+// @Component({
+//   components: {
+//     HelloWorld
+//   }
+// })
+export default class Home extends Vue {}
 </script>
---
+++ src/views/Login.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="login">
+    <div class="login-card card card-body">
+      <h3 class="card-title text-center">Login</h3>
+      <div class="card-text">
+        <form>
+          <div class="form-group mb-3">
+            <label for="username">Username</label>
+            <input type="text" class="form-control" id="username" placeholder="Username">
+          </div>
+          <div class="form-group mb-3">
+            <label for="password">Password</label>
+            <input type="password" class="form-control" id="password" placeholder="Password">
+          </div>
+          <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<style scoped>
+  .login-card {
+    max-width: 24rem;
+    margin: auto;
+  }
+</style>
